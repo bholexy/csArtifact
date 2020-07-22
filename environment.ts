b9c18945c065abd82ecfe7bf2d41045ee0d8b652
Fasugba Boluwatife <fasugbab@gmail.com>
2020-07-22 08:42:27 +0000

    first commit

---
+++ environment.ts
@@ -0,0 +1,21 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  domain_protocol:"http://", 
+  domain_name: "35.168.212.78",
+  slack_client_id: "241971098774.1085436583447",
+  ws_url : "wss://myleag3s8i.execute-api.us-west-2.amazonaws.com/chatscrum/",
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
